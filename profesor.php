66e9c195e76b6bad5b1bb139c53fd644fca9a25f
AdrianEscobar <adrian_080891@hotmail.com>
2011-09-17 02:52:24 -0700

    ahora si vista nitida

--- profesor.php
+++ profesor.php
@@ -34,8 +34,10 @@
 					</div>
 				</a>
 				<div id = 'menu' class = 'menu' align = 'left'>
-					<label> Secciones </label>
-					<div id = 'entrar' align='center'><a href = '#' onclick = 'prueba();'><div class = 'button'>Materia</div></a></div>
+					<div style = "width: 150px; background-color: #868686; height: 30px;"><label style = "font-size:20px; margin-left: 36px;"> Secciones </label></div>
+					<!-- Las materias se generara dinamicamente-->
+					<div id = 'menuboton' align='center'><a href = '#' onclick = 'prueba();'><div class = 'menubutton'>Materia</div></a></div>
+					<div id = 'menuboton' align='center'><a href = '#' onclick = 'prueba();'><div class = 'menubutton'>Materia</div></a></div>
 				</div>
 				<div id = 'vista' class = 'vista' align = 'center'>
 					<label class = 'letras'> Lista Estudiantes</label>
